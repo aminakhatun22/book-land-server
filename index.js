8a04e16bd0460ba60b3b0bf392dd212f2aafbdc4
uziriahimu <uziriahimu@gmail.com>
2022-11-27 23:05:56 +0600

    userAdmin

--- index.js
+++ index.js
@@ -68,6 +68,36 @@ async function run() {
         })
 
 
+        app.get('/users', async (req, res) => {
+            const query = {}
+            const user = await usersCollection.find(query).toArray()
+            res.send(user)
+        })
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        })
+        app.put('/users/admin/:id', async (req, res) => {
+
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+            const option = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, option)
+            res.send(result)
+        })
 
 
     }
